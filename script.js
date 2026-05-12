b1ef6e43020226103a0551c98c371a35087cfd69
karan-joshi-snorlax <scrapruckus67@gmail.com>
2026-05-12 23:24:33 +0530

    adding

---
+++ script.js
@@ -0,0 +1,31 @@
+let inc = document.querySelector(".icon2");
+let count = document.querySelector(".count");
+let dec = document.querySelector(".icon1")
+let v = 0;
+let str = document.querySelector(".rstbtn")
+let timer = document.querySelector(".timer")
+let cntrl = document.querySelector(".strt-btn")
+
+inc.addEventListener("click", function() {
+    v = v + 1;
+    count.innerText = v;
+    localStorage.setItem("count",v)
+});
+dec.addEventListener("click",function(){
+        v=v-1
+    count.innerText = v;
+    localStorage.setItem("count",v)
+})
+
+
+str.addEventListener("click", function() {
+    
+    let po = localStorage.getItem("count");
+
+    count.innerText = po;
+
+});
+cntrl.addEventListener("click",function(){
+    cntrl.
+})
+
